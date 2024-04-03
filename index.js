87e1d09deb47abdcba0eb86928682540fbd8e18a
JANVIVIRADIYA <viradiyajanvi1104@gmail.com>
2024-04-04 02:52:27 +0530

    UDF

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function setFirstName() {
+  document.getElementById("firstname").innerHTML="janvi"
+}
+
+function setLastName() {
+  document.getElementById("lastname").innerHTML="viradiya"
+}
+
+function setFullName() {
+  document.getElementById("fullname").innerHTML="janvi viradiya"
+}
+
+function resetDefaults() {
+  document.getElementById("firstname").innerHTML=""
+  document.getElementById("lastname").innerHTML=""
+  document.getElementById("fullname").innerHTML=""
+}
